a1a8e250babc775d0a72721803a0706088fc6800
rian-hotate <kimakary9@gmail.com>
2021-02-21 15:09:50 +0900

    "firebaseの追加(Android)"

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app7/routes/login_route.dart';
 import 'root.dart';
 import 'routes/Top_route.dart';
 
-void main() => runApp(App());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runZonedGuarded(() {
+    runApp(App());
+  }, (error, stackTrace) {
+    print('runZonedGuarded: Caught error in my root zone.');
+    FirebaseCrashlytics.instance.recordError(error, stackTrace);
+  });
+}
 
 class App extends StatelessWidget {
 
@@ -17,7 +30,7 @@ class App extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/' : (context) => RootWidget(),
+        '/': (context) => RootWidget(),
         '/login_route': (context) => Talk(),
       },
     );
