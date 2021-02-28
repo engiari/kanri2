a92f5db24455cf25eec2ee858723e1b9536648c3
nobu <engiyuka1107@gmail.com>
2021-02-28 16:48:35 +0900

    iosで動くようにした

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
-import 'package:flutter/material.dart';
-import 'root.dart';
-import 'routes/Top_route.dart';
+import 'dart:async';
 
-void main() => runApp(App());
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app7/screen/util/login_route.dart';
+import 'package:flutter_app7/screen/routes/root.dart';
+import 'package:flutter_app7/screen/util/signup_route.dart';
+import 'package:flutter_app7/screen/top_route.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runZonedGuarded(() {
+    runApp(App());
+  }, (error, stackTrace) {
+    print('runZonedGuarded: Caught error in my root zone.');
+    FirebaseCrashlytics.instance.recordError(error, stackTrace);
+  });
+}
 
 class App extends StatelessWidget {
 
@@ -14,10 +29,12 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.blue,
       ),
-      home: RootWidget(),
       initialRoute: '/',
       routes: {
-        //   '/routes/talk_route': (context) => TalkRoute(),
+        '/': (context) => Top(),
+        '/home': (context) => RootWidget(),
+        '/login_route': (context) => LoginPage(),
+        '/signup_route': (context) => SignUpPage(),
       },
     );
   }
---
+++ lib/screen/util/signup_model.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:flutter/material.dart';
+
+class SignUpModel extends ChangeNotifier {
+  String mail = '';
+  String password = '';
+
+  final auth.FirebaseAuth _auth = auth.FirebaseAuth.instance;
+
+  Future signUp() async {
+    if (mail.isEmpty) {
+      throw ('メールアドレスを入力してください');
+    }
+
+    if (password.isEmpty) {
+      throw ('パスワードを入力してください');
+    }
+
+    // todo
+    final user = (await _auth.createUserWithEmailAndPassword(
+      email: mail,
+      password: password,
+    ))
+        .user;
+    final email = user.email;
+
+    FirebaseFirestore.instance.collection('users').add(
+      {
+        'email': email,
+        'createdAt': Timestamp.now(),
+      },
+    );
+  }
+}
