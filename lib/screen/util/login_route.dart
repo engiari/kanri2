40d229a2ed7870b451124afb5b5ae0e1103b164d
engiari <engiyuka1107@gmail.com>
2021-04-04 13:55:17 +0900

    SharedPreferencesとcloud_firestoreの利用

--- lib/screen/util/login_route.dart
+++ lib/screen/util/login_route.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app7/screen/util/login_model.dart';
 import 'package:provider/provider.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginPage extends StatelessWidget {
   @override
